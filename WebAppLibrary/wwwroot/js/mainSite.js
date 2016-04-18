d0448654474fff62c3cbb91318da0bd1c3088ae0
Karen Goytizolo <kgoytizolo@gmail.com>
2016-04-17 23:07:13 -0500

    ASPNET 5 Web App version 1.5 - My Library
    This is the v.1.5 version of My Library ASPNET 5 web application
    (practice mode). Adding javascript files, adding Bower to configure
    client-side dependencies (such as JQuery, Bootstrap, etc) and applying
    JQuery and CSS effects to enrich web app.

---
+++ WebAppLibrary/wwwroot/js/mainSite.js
@@ -0,0 +1,31 @@
+﻿//Code is executed for exclusive use
+//Function outside global scope
+(function () {
+    //var main = $("#main");                                   //JQuery replaces document.getElementById
+    //main.on("mouseenter", function () {                     //On indicates Events
+    //    main.style = "background-color: #FFF;";
+    //});
+    //main.on("mouseleave", function () {                     //On indicates events
+    //    main.style = "background-color: antiquewhite;";
+    //});
+
+    //var menuItems = $("ul.menu li a");                      //Will applicate to ul.menu li a, can manage events in one callback
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());                                   //Gets de text of each clicked menu
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");        //Also define variables with $ to diferentiate from clasic javascript
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        }
+        else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+    
+})();
+
+
